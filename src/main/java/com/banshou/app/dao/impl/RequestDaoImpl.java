a95f9f2b0861523b197a426c62b917e31bb3acdd
Er Qing Zhang <zherq@cn.ibm.com>
2015-05-24 23:04:33 +0800

    完善商店审批功能

--- src/main/java/com/banshou/app/dao/impl/RequestDaoImpl.java
+++ src/main/java/com/banshou/app/dao/impl/RequestDaoImpl.java
@@ -1,9 +1,11 @@
 package com.banshou.app.dao.impl;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
@@ -12,6 +14,9 @@ import org.springframework.stereotype.Repository;
 
 import com.banshou.app.dao.RequestDao;
 import com.banshou.app.domain.Request;
+import com.banshou.app.domain.Store;
+import com.banshou.app.domain.User;
+import com.banshou.app.utils.common.CodeGenerator;
 
 @Repository
 public class RequestDaoImpl implements RequestDao {
@@ -41,6 +46,36 @@ public class RequestDaoImpl implements RequestDao {
 				r.setStatus("In Progress");
 			} else if ("finish".equals(flag)) {
 				r.setStatus("Finished");
+				
+				// 添加商店
+				String storeNumber = "SD" + CodeGenerator.generateTimeStampString();
+				Store s = new Store();
+				s.setAddress("");
+				s.setBrief("");
+				s.setImage("");
+				s.setIsvalid(true);
+				s.setIsvip(false);
+				s.setLatitude("");
+				s.setLongitude("");
+				s.setName("");
+				s.setNumber(storeNumber);
+				s.setPoint(0);
+				s.setTel("");
+				s.setTime(new Date());
+				
+				em.persist(s);
+				
+				// 更新用户信息
+				String sql = "SELECT * FROM bs_user WHERE u_mobile = '" + r.getMobile() + "'";
+				Query query = em.createNativeQuery(sql, User.class);
+				User u = (User) query.getSingleResult();
+				u.setIsstore(true);
+				u.setStoreNum(storeNumber);
+				
+				em.merge(u);
+				em.flush();
+				
+				
 			} else {
 				r.setStatus("Cancelled");
 			}
